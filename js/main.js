ebdd8feca56d3edd726e31e14dd0c3d678626761
ArmAtrix123 <alexstor2003@gmail.com>
2022-09-01 23:29:08 +0300

    fixed: class hotel

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.hotel-slider', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
     // Navigation arrows
     navigation: {
-      nextEl: '.slider-button--next',
-      prevEl: '.slider-button--prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
     },
     effect: 'coverflow',
     keyboard: {
